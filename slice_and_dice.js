79bb83224ebe8a113114d40c4b43e04955049592
jashua-d <denisjas@kean.edu>
2023-11-19 15:28:21 -0800

    final edits

--- slice_and_dice.js
+++ slice_and_dice.js
@@ -6,44 +6,58 @@
 /** remove a random element in the items array
 and return a new array without that item. */
 
-function removeRandom(items) {
-
-}
+function removeRandom(items) {};
 
 const removeRandom = items => {
-    let idx = Math.floor()
-
-}
+    const randomIndex = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, randomIndex), ...items.slice(randomIndex + 1)];
+  };
+  
+  
 /** Return a new array with every item in array1 and array2. */
 
-function extend(array1, array2) {
+function extend(array1, array2) {}
 
-}
+const extend = (array1, array2) => {
+    return [...array1, ...array2];
+  };
+
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
-function addKeyVal(obj, key, val) {
+function addKeyVal(obj, key, val) {}
 
-}
+const addKeyVal = (obj, key, val) => {
 
+    let newObj = { ...obj }
+    newObj[key] = val;
+    return newObj;
+};
 
 /** Return a new object with a key removed. */
 
-function removeKey(obj, key) {
+function removeKey(obj, key) {}
 
-}
-
+const removeKey = (obj, key) => {
+    let newObj = { ...obj }
+    delete newObj[key]
+    return newObj;
+};
 
 /** Combine two objects and return a new object. */
 
-function combine(obj1, obj2) {
+function combine(obj1, obj2) {}
 
-}
+const combine = (obj1, obj2) => {
+    return { ...obj1, ...obj2 };
+  };
 
 
 /** Return a new object with a modified key and value. */
 
-function update(obj, key, val) {
+function update(obj, key, val) {};
 
-}
+let newObj = { ...obj }
+newObj[key] = val;
+return newObj;
